0d0e82ab3af3c4be6649bd6552f9c6b97d27dab2
OlegSavytskyi <oleg.savytsky@gmail.com>
2017-10-04 17:24:36 +0300

    add sing in github

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-var width = 800;
-var height = 600;
+var width = 640;
+var height = 480;
 var app;
 var colors = [0xFFFF0B, 0xFF700B, 0x4286f4, 0x4286f4, 0xf441e8, 0x8dff6d, 0x41ccc9, 0xe03375, 0x95e032, 0x77c687, 0x43ba5b, 0x0ea3ba];
 var gravity = 1;
